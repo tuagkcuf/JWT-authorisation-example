b651da6aa035d61f7bd3f108204ca20a8f290e0e
tuagkcuf <jokersms222@gmail.com>
2023-03-10 18:14:22 +0300

    setup login

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import * as dotenv from "dotenv"
 import db from "./config/database.js"
 import express from "express"
 import User from "./model/user.js"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import auth from "./middleware/auth.js"
 
 dotenv.config()
 db()
@@ -13,7 +16,8 @@ app.use(express.json())
 app.post("/register", async (req, res) => {
     try {
         // get user input
-        const {first_name, last_name, email, password} = req.body
+        const { first_name, last_name, email, password } = req.body
+        console.log(req.body)
 
         // validate input
         if (!(email && password && first_name && last_name)) {
@@ -27,9 +31,9 @@ app.post("/register", async (req, res) => {
         if (oldUser) {
             return res.status(409).send("User already exist. Please login")
         }
-
+ 
         // encrypt our password
-        encryptedPassword = await bcrypt.hash(password, 10)
+        let encryptedPassword = await bcrypt.hash(password, 10)
 
         // create user in our database
         const user = await User.create({
@@ -57,8 +61,43 @@ app.post("/register", async (req, res) => {
     }
 })
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
+    try {
+        // get user input 
+        const { email, password } = req.body
 
+        // validate user input
+        if (!(email && password)) {
+            res.status(400).send("All input is required")
+        }
+
+        // validate if user already exists
+        const user = await User.findOne({ email })
+
+        if (user && (bcrypt.compare(password, user.password))) {
+            // create token
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "2h",
+                },
+            )
+
+            // save user token
+            user.token = token
+
+            res.status(200).json(user)
+        }
+
+        res.status(400).send("Invalid Creadntials")
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.post("/welcome", auth, (req, res) => {
+    res.status(200).send("Welcome")
 })
 
 export default app
